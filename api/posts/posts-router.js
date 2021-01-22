f483ba72466b16827c46e2517523b67309186284
unknown <erickvillegasecheverria@gmail.com>
2021-01-21 20:31:11 -0800

    MVP

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,24 +1,58 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+const db = require("./posts-model");
+
+const {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePostId,
+  validatePost,
+} = require("../middleware/middleware");
+
+router.get("/", async (req, res) => {
+  try {
+    const posts = await db.get();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json({ error: "Server problem" });
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", validatePostId, async (req, res) => {
   // do your magic!
   // this needs a middleware to verify post id
+  try {
+    const post = await db.getById(req.params.id);
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({ error: "Server problem" });
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", validatePostId, async (req, res) => {
   // do your magic!
   // this needs a middleware to verify post id
+  try {
+    const post = await db.remove(req.params.id);
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({ error: "Server problem" });
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", validatePostId, validatePost, async (req, res) => {
   // do your magic!
   // this needs a middleware to verify post id
+  try {
+    const post = await db.update(req.params.id);
+    res.status(201).json(post);
+  } catch (err) {
+    res.status(500).json({ error: "Server problem" });
+  }
 });
 
 // do not forget to export the router
+module.exports = router;
